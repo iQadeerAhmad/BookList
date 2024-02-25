b8c5575e46cfec9a87f96018a4682fbc4730509a
Qadeer Ahmad <qadeerahmad294gb@gmail.com>
2024-02-25 23:46:53 +0500

    added js and css and modified html
    js- added whole functionality of the web app
    css- added style to the web app
    html- modified some tags

---
+++ main.js
@@ -0,0 +1,35 @@
+const title = document.getElementById('title');
+const author = document.getElementById('author');
+const year = document.getElementById('year');
+const bookList = document.getElementById('book-list');
+const btn = document.querySelector('.btn');
+
+btn.addEventListener('click', function (e) {
+    e.preventDefault()
+
+    
+    
+    let titleData=document.createElement("div");
+    titleData.innerText=title.value;
+    let authorData=document.createElement("div");
+    authorData.innerText=author.value;
+    let yearData=document.createElement('div');
+    yearData.innerText=year.value;
+
+    if (title.value === '' || author.value === '' || year.value === '') {
+        
+        alert('Please fill in all the fields');
+    } else {
+
+    let bookTable=document.createElement("section");
+    bookTable.className="table-section";
+    
+
+    bookTable.appendChild(titleData)
+    bookTable.appendChild(authorData)
+    bookTable.appendChild(yearData)
+
+    document.getElementById('book-list').appendChild(bookTable)
+    }
+    
+});
